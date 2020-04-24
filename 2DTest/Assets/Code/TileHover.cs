1bcbbe233942112c65adfd8c8851514c37a233c5
nighthawk30 <39445706+nighthawk30@users.noreply.github.com>
2020-04-23 17:08:49 -0700

    Code and Sprites to detect if the cursor is hovering over a tile

---
+++ 2DTest/Assets/Code/TileHover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class TileHover : MonoBehaviour
+{
+    public Sprite BuildTile;
+    public Sprite HoverTile;
+    bool hovering;
+    float screenRatio;
+
+    Vector2 mouseGamePostion = new Vector2(0, 0);
+    Vector2 cursorDistance = new Vector2(0, 0);
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenRatio = 288 / ((float)(Screen.height));
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseGamePostion.x = (Input.mousePosition.x - Screen.width / 2) * screenRatio;
+        mouseGamePostion.y = (Input.mousePosition.y - Screen.height / 2) * screenRatio;
+
+        cursorDistance.x = Mathf.Abs(mouseGamePostion.x - transform.position.x);
+        cursorDistance.y = Mathf.Abs(mouseGamePostion.y - transform.position.y);
+
+        if (!hovering && cursorDistance.x < 32 && cursorDistance.y < 32)//32 bc the object is scaled by 2
+        {
+            spriteRenderer.sprite = HoverTile;
+            hovering = true;
+        }
+        
+        if (hovering && (cursorDistance.x > 32 || cursorDistance.y > 32))
+        {
+            spriteRenderer.sprite = BuildTile;
+            hovering = false;
+        }
+               
+    }
+}
