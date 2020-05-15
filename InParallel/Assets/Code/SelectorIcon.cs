ac34b0151d49b36edf220fba5f011eb19a35e366
nighthawk30 <39445706+nighthawk30@users.noreply.github.com>
2020-05-15 16:22:09 -0700

    Basic Tooltip Functional

--- InParallel/Assets/Code/SelectorIcon.cs
+++ InParallel/Assets/Code/SelectorIcon.cs
@@ -8,7 +8,22 @@ public class SelectorIcon : MonoBehaviour
 {
     public GameObject tile;//a reference to the specific  tile that this selector uses
     public GameObject controller;
+    string tool;
     public int iconType;
+    bool enter = false;//keeps track of when the mouse enters the icon to show the tool tip
+
+    void Start()
+    {
+        switch (iconType)//set the tool type
+        {
+            case 0:
+                tool = "Battery";
+                break;
+            case 1:
+                tool = "Light";
+                break;
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,6 +36,18 @@ public class SelectorIcon : MonoBehaviour
             vroom.transform.SetSiblingIndex(transform.parent.GetSiblingIndex() + 1);//put tiles at top so drawing order is followed
             vroom.GetComponent<RectTransform>().sizeDelta = transform.parent.GetComponent<GridLayoutGroup>().cellSize;//dynamically set size of dragtile to fit that of the cell
             vroom.GetComponent<Tile>().controller = controller;//give the tile a reference to the controller
+        }
+
+        //tooltip 
+        if (GetComponent<Hover>().HoverTest() && !enter)
+        {
+            enter = true;
+            controller.GetComponent<Controller>().tip.GetComponent<Tooltip>().ShowTooltip(tool);
+        }
+        if (enter && !GetComponent<Hover>().HoverTest())
+        {
+            enter = false;
+            controller.GetComponent<Controller>().tip.GetComponent<Tooltip>().HideTooltip();
         }
     }
 }
