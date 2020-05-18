7a5c8e06893c83dff2ff7c5268c5d0eac11bc779
nighthawk30 <39445706+nighthawk30@users.noreply.github.com>
2020-05-18 15:50:54 -0700

    Setting up the first level

---
+++ InParallel/Assets/Code/Controller1.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller1 : MonoBehaviour
+{
+    //to create selector icons but might need 1 for each depending on how its done, hopefully can just change sprite and name
+    public GameObject batteryIcon;
+    public GameObject copperIcon;
+    public GameObject zincIcon;
+    [SerializeField] GridSlot gridSlot;//to create grid tiles
+    //public GameObject wireToggle;
+    public GameObject buildButton;
+    public GameObject craftTile;
+    public GameObject toolTip;
+
+    public GameObject currentSelection = null;//what the user is selecting this a way of passing references between objects
+
+    GridSlot grid;
+    int gridSize = 2;
+    public GameObject[] board;
+    bool corunning = false;
+
+    // Start is called before the first frame update
+    void Start()//build ui
+    {
+        board = new GameObject[gridSize];
+        //add all tiles to grid
+        for (int i = 0; i < gridSize; i++)
+        {
+            //Instantiate(Object original, Transform parent);
+            grid = Instantiate(gridSlot, transform.parent.GetChild(1).GetChild(0));//slot it into the second child of the canvas, the grid, dont adjust scale
+            grid.gameObject.GetComponent<GridSlot>().controller = this.gameObject;//tell the tile that this is the controller
+            board[i] = grid.gameObject;
+        }
+        //Add Selector Tiles
+        GameObject zinc = Instantiate(zincIcon, transform.parent.GetChild(1).GetChild(1));//slot it into the third child of the canvas, the tool panel, dont adjust scale
+        zinc.gameObject.GetComponent<SelectorIcon>().controller = this.gameObject;//tell the selector that this is the controller
+        GameObject copper = Instantiate(copperIcon, transform.parent.GetChild(1).GetChild(1));//slot it into the third child of the canvas, the tool panel, dont adjust scale
+        copper.gameObject.GetComponent<SelectorIcon>().controller = this.gameObject;
+        //Add Craft Window
+        GameObject craft = Instantiate(craftTile, transform.parent.GetChild(1).GetChild(2));//output slot
+        craft.gameObject.GetComponent<CraftTile>().controller = this.gameObject;//tell the tile that this is the controller
+        //Add tooltip ui
+        toolTip = Instantiate(toolTip, transform.parent.GetChild(4));//Draw layer +4 - topmost
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
+        //testing
+        if (Input.GetMouseButtonUp(0))//intervals and you arent placing wires
+        {
+            StartCoroutine(DelayBuild(0.1f));
+        }
+    }
+
+    IEnumerator DelayBuild(float time)
+    {
+        if (corunning)
+        {
+            yield break;
+        }
+        corunning = true;
+        yield return new WaitForSeconds(time);
+        //
+        GetComponent<BuildCircuit>().Build();
+        //
+        corunning = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();//for builds
+        //UnityEditor.EditorApplication.isPlaying = false;//for editor
+    }
+}
